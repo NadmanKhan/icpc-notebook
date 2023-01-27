74172feb73a9c365387c9e7efb44a641f5d287fb
Nadman Ashraf Khan <nadman.ash.khan@gmail.com>
2023-01-27 22:24:44 +0600

    .

--- src/---/Stress Test - Linux [SA].cpp
+++
@@ -1,7 +0,0 @@
-for((i = 1; i <= 1000; ++i)); do
-    echo Testing $i
-    ./gen > in.txt
-    ./main < in.txt > out1.txt
-    ./brute < in.txt > out2.txt
-    diff -w out1.txt out2.txt || break
-done
